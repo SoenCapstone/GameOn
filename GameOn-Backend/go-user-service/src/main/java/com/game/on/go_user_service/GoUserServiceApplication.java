25aa25f9f350f5b338355b45a7e5e45d6757a24a
Zachariya Javeri <zach.javeri@gmail.com>
2025-09-29 22:18:58 -0400

    Added Lombock dependency to backend projects, this allows us to use the Log4j2 library in our library. Added some logging statements when the application starts to show it is working.

--- GameOn-Backend/go-user-service/src/main/java/com/game/on/go_user_service/GoUserServiceApplication.java
+++ GameOn-Backend/go-user-service/src/main/java/com/game/on/go_user_service/GoUserServiceApplication.java
@@ -1,13 +1,22 @@
 package com.game.on.go_user_service;
 
+import lombok.extern.log4j.Log4j2;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Log4j2
 public class GoUserServiceApplication {
 
-	public static void main(String[] args) {
+    private static final Logger log = LoggerFactory.getLogger(GoUserServiceApplication.class);
+
+    public static void main(String[] args) {
 		SpringApplication.run(GoUserServiceApplication.class, args);
+
+        log.info("Basic information log");
+        log.error("Error message");
 	}
 
 }
