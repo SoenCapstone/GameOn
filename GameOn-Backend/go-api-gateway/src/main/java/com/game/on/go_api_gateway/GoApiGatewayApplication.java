4345feabc8ce4e6f4b26e747643380a5ed389477
Luca <100626835+GianlucaGirardi@users.noreply.github.com>
2025-10-05 11:19:36 -0400

    GO-74: Added api-gateway (#75)

---
+++ GameOn-Backend/go-api-gateway/src/main/java/com/game/on/go_api_gateway/GoApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.game.on.go_api_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoApiGatewayApplication.class, args);
+	}
+
+}
