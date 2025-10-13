e3839597e70800ebf4eb758e8fbc823ca0513861
Johnny-Aldeb <johnny.aldeb@gmail.com>
2025-10-13 12:54:33 -0400

    test fix

--- GameOn-Backend/go-user-service/src/test/java/com/game/on/go_user_service/UserServiceTest.java
+++ GameOn-Backend/go-user-service/src/test/java/com/game/on/go_user_service/UserServiceTest.java
@@ -17,6 +17,8 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.junit.jupiter.MockitoSettings;
+import org.mockito.quality.Strictness;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
@@ -36,6 +38,7 @@ import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
+@MockitoSettings(strictness = Strictness.LENIENT)
 class UserServiceTest {
 
     @InjectMocks
