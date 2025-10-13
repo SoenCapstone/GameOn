1c19d9c81868614fb8e8210eeb294b4b6c874fc4
Danny Mousa <dannymousa1999@hotmail.com>
2025-10-13 19:56:33 -0400

    Added annotation to constant

--- GameOn-Backend/go-user-service/src/main/java/com/game/on/go_user_service/constants/KeycloakConstants.java
+++ GameOn-Backend/go-user-service/src/main/java/com/game/on/go_user_service/constants/KeycloakConstants.java
@@ -1,5 +1,9 @@
 package com.game.on.go_user_service.constants;
 
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class KeycloakConstants {
     public static final String CREDENTIAL_TYPE = "password";
     public static final boolean TEMPORARY = false;
