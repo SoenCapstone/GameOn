cd659dd6dc7703fc67bddc48f90a16ef95ee57d7
Zachariya Javeri <zach.javeri@gmail.com>
2025-10-13 22:06:03 -0400

    Remove unused dependency and cleanup old log messages. Update backend yaml to include common

--- GameOn-Backend/go-config-server/src/main/java/com/game/on/go_config_server/GoConfigServerApplication.java
+++ GameOn-Backend/go-config-server/src/main/java/com/game/on/go_config_server/GoConfigServerApplication.java
@@ -1,8 +1,6 @@
 package com.game.on.go_config_server;
 
 import lombok.extern.log4j.Log4j2;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
@@ -11,8 +9,6 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 @Log4j2
 @EnableConfigServer
 public class GoConfigServerApplication {
-
-    private static final Logger log = LoggerFactory.getLogger(GoConfigServerApplication.class);
 
     public static void main(String[] args) {
 		SpringApplication.run(GoConfigServerApplication.class, args);
