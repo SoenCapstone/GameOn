13b91c2fbe25a754f112fdb487219e5c6d51f961
Mahmoud Mohamed <mahmoud@null.net>
2025-10-16 21:50:31 -0400

    Created a global header component

---
+++ GameOn-Frontend/components/header.tsx
@@ -0,0 +1,25 @@
+import { View } from "react-native";
+import React from "react";
+
+type HeaderProps = {
+    left?: React.ReactNode;
+    center: React.ReactNode;
+    right?: React.ReactNode;
+};
+
+export default function Header({ left, center, right }: HeaderProps) {
+    return (
+        <View
+            style={{
+                width: "100%",
+                height: "100%",
+                flexDirection: "row",
+                alignItems: "center",
+            }}
+        >
+            <View style={{ flex: 1, alignItems: "flex-start" }}>{left}</View>
+            <View style={{ flex: 1, alignItems: "center" }}>{center}</View>
+            <View style={{ flex: 1, alignItems: "flex-end" }}>{right}</View>
+        </View>
+    );
+}
