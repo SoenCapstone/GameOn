189ab9204cd0daa51d4fbd344feeb50653c3ad0c
Mahmoud Mohamed <mahmoud@null.net>
2025-10-17 19:04:08 -0400

    Created a logo, page title, and a header button components

--- GameOn-Frontend/components/logo.tsx
+++ GameOn-Frontend/components/logo.tsx
@@ -3,7 +3,7 @@ import { Image } from "expo-image";
 
 export function Logo() {
   return (
-    <GlassView style={{ width: 44, height: 44, borderRadius: 12 }}>
+    <GlassView style={{ width: 44, height: 44, borderRadius: 14 }}>
       <Image
         source={require("@/assets/images/logo.png")}
         style={{ position: "absolute", top: 0, right: 0, bottom: 0, left: 0 }}
