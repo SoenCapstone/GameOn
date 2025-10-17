b27dcbcb804feafae4a32eb5b00db8b1b37aa1f2
Mahmoud Mohamed <mahmoud@null.net>
2025-10-17 17:23:40 -0400

    Added native tabs

--- GameOn-Frontend/app/(tabs)/_layout.tsx
+++ GameOn-Frontend/app/(tabs)/_layout.tsx
@@ -1,43 +1,17 @@
-import { Tabs } from 'expo-router';
-import React from 'react';
-
-import { HapticTab } from '@/components/haptic-tab';
-import { IconSymbol } from '@/components/ui/icon-symbol';
-import { Colors } from '@/constants/theme';
-import { useColorScheme } from '@/hooks/use-color-scheme';
-
-const HomeIcon = ({ color }: { color: string }) => (
-  <IconSymbol size={28} name="house.fill" color={color} />
-);
-
-const ExploreIcon = ({ color }: { color: string }) => (
-  <IconSymbol size={28} name="paperplane.fill" color={color} />
-);
+import React from "react";
+import { NativeTabs, Label, Icon } from "expo-router/unstable-native-tabs";
 
 export default function TabLayout() {
-  const colorScheme = useColorScheme();
-
   return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-        tabBarButton: HapticTab,
-      }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Home',
-          tabBarIcon: HomeIcon,
-        }}
-      />
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: 'Explore',
-          tabBarIcon: ExploreIcon,
-        }}
-      />
-    </Tabs>
+    <NativeTabs>
+      <NativeTabs.Trigger name="index">
+        <Label>Home</Label>
+        <Icon sf="house.fill" />
+      </NativeTabs.Trigger>
+      <NativeTabs.Trigger name="explore">
+        <Label>Home</Label>
+        <Icon sf="paperplane.fill" />
+      </NativeTabs.Trigger>
+    </NativeTabs>
   );
 }
