7c6f4ffc6c1e27c99a6ac9d6f32aa4a047929b3e
Mahmoud Mohamed <mahmoud@null.net>
2025-10-16 21:59:44 -0400

    Created a page title component to be used alongside the header component to display the title of the page

---
+++ GameOn-Frontend/components/page-title.tsx
@@ -0,0 +1,15 @@
+import { View, Text } from "react-native";
+
+interface TitleProps {
+  title: string;
+}
+
+export default function PageTitle({ title }: TitleProps) {
+  return (
+    <View style={{ justifyContent: "center" }}>
+      <Text style={{ fontSize: 17, fontWeight: "600", color: "#FFF" }}>
+        {title}
+      </Text>
+    </View>
+  );
+}
