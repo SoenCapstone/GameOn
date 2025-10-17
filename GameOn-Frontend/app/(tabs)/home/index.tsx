761b252935e0cc9f173b64ea34ac75772d352f4f
Mahmoud Mohamed <mahmoud@null.net>
2025-10-16 22:44:59 -0400

    Used a custom background component to display a gradient

--- GameOn-Frontend/app/(tabs)/home/index.tsx
+++ GameOn-Frontend/app/(tabs)/home/index.tsx
@@ -1,22 +1,11 @@
-import React from 'react';
-import { SafeAreaView } from 'react-native-safe-area-context';
-import { searchStyles } from '@/components/SearchPage/constants';
-import { LinearGradient } from 'expo-linear-gradient';
+import React from "react";
+import ContentArea from "@/components/content-area";
+import { Background } from "@/components/background";
 
 export default function Home() {
-
   return (
-    <SafeAreaView style={searchStyles.container}>
-
-      <LinearGradient
-              colors={['#0C456E', 'rgba(0,0,0,0)']}
-              start={{ x: 0, y: 0 }}
-              end={{ x: 0, y: 1 }}
-              style={searchStyles.topGradient}
-              pointerEvents="none"
-            />
-      
-
-    </SafeAreaView>
+    <ContentArea>
+      <Background preset="blue" mode="default" />
+    </ContentArea>
   );
 }
