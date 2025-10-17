2e5125e9d7b8adf26c41e5f0aaee47f4b8a14f04
Mahmoud Mohamed <mahmoud@null.net>
2025-10-16 22:48:27 -0400

    Removed unnecessary pages from the tabs layout

--- GameOn-Frontend/app/(tabs)/_layout.tsx
+++ GameOn-Frontend/app/(tabs)/_layout.tsx
@@ -1,54 +1,16 @@
-import { NativeTabs, Label, Icon } from 'expo-router/unstable-native-tabs';
-import React, { useEffect, useState } from 'react';
-import { SearchProvider } from '@/contexts/SearchContext';
-import { subscribe, getOverlayHeight } from '@/utils/tabbar-visibility';
-
-import { useColorScheme } from '@/hooks/use-color-scheme';
-import { View } from 'react-native';
-import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { NativeTabs, Label, Icon } from "expo-router/unstable-native-tabs";
+import React from "react";
+import { View } from "react-native";
 
 export default function TabLayout() {
-  useColorScheme();
-
-  const [hidden, setHidden] = useState(false);
-  const insets = useSafeAreaInsets();
-
-  useEffect(() => {
-    const unsub = subscribe(setHidden);
-    return unsub;
-  }, []);
-
   return (
-    <SearchProvider>
-      <View style={{ flex: 1 }}>
-        <NativeTabs>
-      <NativeTabs.Trigger
-        name="index">
+    <View style={{ flex: 1 }}>
+      <NativeTabs>
+        <NativeTabs.Trigger name="home">
           <Label>Home</Label>
-          <Icon sf="house.fill"/>
-      </NativeTabs.Trigger>
-      <NativeTabs.Trigger
-        name="home">
-          <Label>Main</Label>
-          <Icon sf="house.fill"/>
-      </NativeTabs.Trigger>
-        </NativeTabs>
-        {hidden && (
-          <View
-            style={{
-              position: 'absolute',
-              left: 0,
-              right: 0,
-              bottom: 0,
-              height: getOverlayHeight(insets.bottom),
-              backgroundColor: '#000',
-              zIndex: 9999,
-              elevation: 9999,
-            }}
-            pointerEvents="auto"
-          />
-        )}
-      </View>
-    </SearchProvider>
+          <Icon sf="house.fill" />
+        </NativeTabs.Trigger>
+      </NativeTabs>
+    </View>
   );
 }
