f599492d6e3415fe580b79e9fa8d87b8fd12071c
Mahmoud Mohamed <mahmoud@null.net>
2025-10-25 00:41:41 -0400

    Fixed glass interactivity effect

--- GameOn-Frontend/components/ui/card.tsx
+++ GameOn-Frontend/components/ui/card.tsx
@@ -27,9 +27,5 @@ const styles = StyleSheet.create({
     width: "100%",
     padding: 24,
     borderRadius: 34,
-    overflow: "hidden",
-    borderStyle: "solid",
-    borderColor: "rgba(108,108,113,0.1)",
-    borderWidth: 1,
   },
 });
