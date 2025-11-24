5f681177c66591aebdbbc4e62e7ed9534c8cb16e
Zachariya Javeri <zach.javeri@gmail.com>
2025-11-23 22:10:33 -0500

    Ignore team service tests for now, Added some new tests for AbstractFeatureFlags

--- GameOn-Backend/common/src/test/java/com/game/on/common/feature_flags/AbstractFeatureFlagsTests.java
+++ GameOn-Backend/common/src/test/java/com/game/on/common/feature_flags/AbstractFeatureFlagsTests.java
@@ -4,13 +4,23 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class AbstractFeatureFlagsTests {
+
+    private static final String FILE_NAME = "TestFlags.json";
+    private static final Path FEATURE_FLAG_FILE_PATH = Paths.get("feature-flags").resolve(FILE_NAME);
 
     private TestFeatureFlags flags;
 
     @BeforeEach
     public void setup() {
         flags = new TestFeatureFlags();
+
+        removeTestFile();
     }
 
     @Test
@@ -23,14 +33,99 @@ public class AbstractFeatureFlagsTests {
         Assertions.assertTrue(flags.isFlagEnabled(TestFlags.ENABLED_BY_DEFAULT));
     }
 
-    @FeatureFlagFile(FileName = "TestFlags.json")
+    @Test
+    public void CreatesDefaultFeatureFlagFile() {
+        Assertions.assertFalse(Files.exists(FEATURE_FLAG_FILE_PATH));
+
+        flags.initialize();
+
+        Assertions.assertTrue(Files.exists(FEATURE_FLAG_FILE_PATH));
+
+        String content = readCurrentFeatureFlagFile();
+
+        Assertions.assertTrue(content.contains("ENABLED_BY_DEFAULT"));
+        Assertions.assertTrue(content.contains("FLAG_ONE"));
+    }
+
+    @Test
+    public void AddsMissingFeatureFlagsUponInitialization() {
+        String testFileStr = "[ {\n" +
+                "  \"key\" : \"FLAG_ONE\",\n" +
+                "  \"enabled\" : false\n" +
+                "} ]";
+
+        writeTestFile(testFileStr);
+
+        // Force re-initialization
+        flags.initialize();
+
+        String testFileContents = readCurrentFeatureFlagFile();
+
+        Assertions.assertTrue(testFileContents.contains("ENABLED_BY_DEFAULT"));
+    }
+
+    @Test
+    public void SkipsFeatureFlagsWithoutADescriptor() {
+        String testFileStr = "[ {\n" +
+                "  \"key\" : \"FLAG_ONE\",\n" +
+                "  \"enabled\" : false\n" +
+                "} ]";
+
+        writeTestFile(testFileStr);
+
+        // Force re-initialization
+        flags.initialize();
+
+        String testFileContents = readCurrentFeatureFlagFile();
+
+        Assertions.assertTrue(testFileContents.contains("ENABLED_BY_DEFAULT"));
+        Assertions.assertFalse(testFileContents.contains("MISSING_DESCRIPTOR"));
+    }
+
+    @Test
+    public void PersistsFeatureFlags() {
+        //Re-create the default file
+        flags.initialize();
+
+        Assertions.assertTrue(Files.exists(FEATURE_FLAG_FILE_PATH));
+
+        boolean result = flags.setFlag(TestFlags.ENABLED_BY_DEFAULT, false);
+
+        Assertions.assertTrue(result);
+
+        flags.initialize();
+
+        boolean resultAfterRefresh = flags.isFlagEnabled(TestFlags.ENABLED_BY_DEFAULT);
+
+        Assertions.assertFalse(resultAfterRefresh);
+    }
+
+    @Test
+    public void UpdateNonExistentFlagReturnsFalse() {
+
+        //Without a descriptor, this value will never appear in the flags
+        boolean result = flags.setFlag(TestFlags.MISSING_DESCRIPTOR, true);
+
+        Assertions.assertFalse(result);
+    }
+
+    @Test
+    public void GetNonExistentFlagReturnsFalse() {
+        boolean res = flags.isFlagEnabled(TestFlags.MISSING_DESCRIPTOR);
+
+        Assertions.assertFalse(res);
+    }
+
+    @FeatureFlagFile(FileName = FILE_NAME)
     private enum TestFlags implements FeatureFlagList<TestFlags> {
 
         @FeatureFlagDescriptor(Name = "Flag one")
         FLAG_ONE,
 
         @FeatureFlagDescriptor(Name = "Enabled by default", DefaultEnabled = true)
-        ENABLED_BY_DEFAULT;
+        ENABLED_BY_DEFAULT,
+
+        MISSING_DESCRIPTOR;
 
         @Override
         public Class<TestFlags> getEnumClass() {
@@ -42,6 +137,34 @@ public class AbstractFeatureFlagsTests {
 
         public TestFeatureFlags() {
             super(TestFlags.class);
+        }
+    }
+
+    public void removeTestFile() {
+        try {
+
+            Files.deleteIfExists(FEATURE_FLAG_FILE_PATH);
+
+        } catch (IOException e) {
+            Assertions.fail("Unable to remove test file before test begins");
+        }
+    }
+
+    public void writeTestFile(String content) {
+
+        try {
+            Files.write(FEATURE_FLAG_FILE_PATH, content.getBytes());
+        } catch (IOException e) {
+            Assertions.fail("Unable to write to test file as desired");
+        }
+    }
+
+    public String readCurrentFeatureFlagFile() {
+        try {
+            return Files.readString(FEATURE_FLAG_FILE_PATH);
+        } catch (IOException e) {
+            Assertions.fail("Unable to read contents of test file");
+            return "";
         }
     }
 }
