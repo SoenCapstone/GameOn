dd075cbc68a7c8d0bb4a61f5c8648e4a9c69089f
Mahmoud M. <76411398+mavmoud@users.noreply.github.com>
2025-12-29 00:35:42 +0300

    GH-182: [style] Changed form input item text color to white

--- Frontend/components/form/input-item.tsx
+++ Frontend/components/form/input-item.tsx
@@ -33,8 +33,7 @@ const styles = StyleSheet.create({
   },
   input: {
     flex: 1,
-    height: "100%",
-    color: "#AFAFB6",
+    color: "white",
     fontSize: 17,
     paddingLeft: 10,
     textAlign: "right",
