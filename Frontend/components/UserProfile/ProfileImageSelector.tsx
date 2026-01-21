5c20c847bf1fecdd58e4e922987b85b0f5e919a6
mariabalanjian <maria.balanjian@gmail.com>
2026-01-21 14:57:43 -0500

    refactor(frontend): remove any from profile image selector

--- Frontend/components/UserProfile/ProfileImageSelector.tsx
+++ Frontend/components/UserProfile/ProfileImageSelector.tsx
@@ -4,9 +4,11 @@ import { ThemedText } from "@/components/themed-text";
 import { pickImage } from "@/components/UserProfile/profile-utils";
 import { profileStyles } from "@/components/UserProfile/profile-styles";
 
+type ProfilePic = { uri: string };
+
 interface Props {
-  profilePic: any;
-  setProfilePic: (pic: any) => void;
+  profilePic: ProfilePic;
+  setProfilePic: (pic: ProfilePic) => void;
 }
 
 export const ProfileImageSelector = ({ profilePic, setProfilePic }: Props) => {
