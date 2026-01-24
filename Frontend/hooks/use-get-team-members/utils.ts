a6ea0a9adea48a1e57f0e672159aed76543bf70c
mariabalanjian <maria.balanjian@gmail.com>
2026-01-24 15:54:18 -0500

    replace any with proper types in fetchTeamMembers

--- Frontend/hooks/use-get-team-members/utils.ts
+++ Frontend/hooks/use-get-team-members/utils.ts
@@ -1,10 +1,17 @@
 import { GO_TEAM_SERVICE_ROUTES } from "../use-axios-clerk";
+import type { AxiosInstance, AxiosResponse } from "axios";
+import { TeamMember } from "./model";
 
-export const fetchTeamMembers =  (teamId: string, api: any) => {
-    return api.get(GO_TEAM_SERVICE_ROUTES.GET_TEAM_MEMBERS(teamId)).then((res: any) =>
-        (res.data ?? []).map((member: any) => ({
-            ...member,
-            id: member.id ?? member.userId,
-        })),
+export const fetchTeamMembers = (
+  teamId: string,
+  api: AxiosInstance,
+): Promise<TeamMember[]> => {
+  return api
+    .get(GO_TEAM_SERVICE_ROUTES.GET_TEAM_MEMBERS(teamId))
+    .then((res: AxiosResponse<TeamMember[]>) =>
+      (res.data ?? []).map((member: TeamMember) => ({
+        ...member,
+        id: member.id ?? member.userId,
+      })),
     );
-}
+};
