dc2c32eae8005416cbb49a0a7f7699d2dcdeaa3b
Mahmoud M. <76411398+mavmoud@users.noreply.github.com>
2026-01-28 20:27:27 -0500

    GH-267: [refactor] Simplify useUpsertUser mutation and reduce retry config

--- Frontend/components/sign-up/hooks/use-insert-clerk-to-be.ts
+++ Frontend/components/sign-up/hooks/use-insert-clerk-to-be.ts
@@ -1,34 +1,20 @@
 import { useMutation } from "@tanstack/react-query";
-import { useUser } from "@clerk/clerk-expo";
-import type { UserResource } from "@clerk/types";
 import { toast } from "@/components/sign-up/utils";
 import {
   GO_USER_SERVICE_ROUTES,
   useAxiosWithClerk,
 } from "@/hooks/use-axios-clerk";
-import {
-  SIGN_UP_SUCCESS_MESSAGE,
-  SIGN_UP_BACKEND_ERROR_MESSAGE,
-} from "@/components/sign-up/constants";
+import { SIGN_UP_SUCCESS_MESSAGE } from "@/components/sign-up/constants";
 
 export const useUpsertUser = () => {
-  const { user } = useUser();
   const api = useAxiosWithClerk();
 
   return useMutation({
-    retry: 3,
-    retryDelay: 400,
+    retry: 1,
+    retryDelay: 100,
     mutationFn: async (payload) => {
       return await api.post(GO_USER_SERVICE_ROUTES.CREATE, payload);
     },
     onSuccess: () => toast(SIGN_UP_SUCCESS_MESSAGE),
-    onError: () => signOutClerkAndDisplayErrorToast(user),
   });
-};
-
-const signOutClerkAndDisplayErrorToast = async (
-  user: UserResource | null | undefined,
-) => {
-  await user?.delete();
-  toast(SIGN_UP_BACKEND_ERROR_MESSAGE);
 };
