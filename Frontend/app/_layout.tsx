f5ab0cc0a4ee7f1b25b83ee588e2344d24db50f6
Mahmoud M. <76411398+mavmoud@users.noreply.github.com>
2026-03-12 04:28:23 -0400

    GH-339: [feat] Prevent splash screen auto hide before clerk loads, and add fade animation

--- Frontend/app/_layout.tsx
+++ Frontend/app/_layout.tsx
@@ -1,6 +1,8 @@
+import { useEffect } from "react";
 import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 import * as SystemUI from "expo-system-ui";
+import * as SplashScreen from "expo-splash-screen";
 import { useAuth } from "@clerk/clerk-expo";
 import TimeAgo from "javascript-time-ago";
 import en from "javascript-time-ago/locale/en.json";
@@ -9,17 +11,28 @@ import { Providers } from "@/contexts/providers";
 TimeAgo.addDefaultLocale(en);
 void SystemUI.setBackgroundColorAsync("black");
 
+void SplashScreen.preventAutoHideAsync();
+SplashScreen.setOptions({
+  fade: true,
+});
+
 function RootStack() {
   const { isLoaded, isSignedIn } = useAuth();
+
+  useEffect(() => {
+    if (isLoaded) {
+      SplashScreen.hide();
+    }
+  }, [isLoaded]);
 
   if (!isLoaded) {
     return null;
   }
 
   return (
-    <Stack>
+    <Stack screenOptions={{ headerShown: false }}>
       <Stack.Protected guard={!isSignedIn}>
-        <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+        <Stack.Screen name="(auth)" />
       </Stack.Protected>
       <Stack.Protected guard={Boolean(isSignedIn)}>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
