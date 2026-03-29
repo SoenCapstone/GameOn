150f6ff34cbc05d0510bb6fa87fb1419b8b61e1d
Gianluca Girardi <gl.girardi1@gmail.com>
2026-03-29 16:42:34 -0400

    GH-384: Addressed League standings defect

--- Backend/go-league-service/src/main/java/com/game/on/go_league_service/league/model/StandingStrategy.java
+++ Backend/go-league-service/src/main/java/com/game/on/go_league_service/league/model/StandingStrategy.java
@@ -1,9 +1,11 @@
 package com.game.on.go_league_service.league.model;
 
+import com.game.on.go_league_service.client.dto.TeamSummaryResponse;
+
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 
 public interface StandingStrategy {
-    List<StandingScore> calculateStanding(List<UUID> teamIds, List<LeagueMatch> matches, Map<UUID, LeagueMatchScore> scoresByMatchId);
+    List<StandingScore> calculateStanding(List<TeamSummaryResponse> teams, List<LeagueMatch> matches, Map<UUID, LeagueMatchScore> scoresByMatchId);
 }
