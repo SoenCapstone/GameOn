d22e7824e37e88bd3dc9ece94d6715db90a03af7
Mahmoud M. <76411398+mavmoud@users.noreply.github.com>
2026-04-04 17:33:50 -0400

    GH-394: [refactor] Remove MaterialIcons fallback from IconSymbol

--- Frontend/components/ui/icon-symbol.tsx
+++ Frontend/components/ui/icon-symbol.tsx
@@ -1,52 +1,32 @@
-// Fallback for using MaterialIcons on Android and web.
+import { SymbolView, SymbolViewProps, SymbolWeight } from "expo-symbols";
+import { StyleProp, ViewStyle } from "react-native";
 
-import MaterialIcons from "@expo/vector-icons/MaterialIcons";
-import { ComponentProps } from "react";
-import { OpaqueColorValue, type StyleProp, type TextStyle } from "react-native";
-
-type MaterialIconName = ComponentProps<typeof MaterialIcons>["name"];
-
-/**
- * Add your SF Symbols to Material Icons mappings here.
- * - see Material Icons in the [Icons Directory](https://icons.expo.fyi).
- * - see SF Symbols in the [SF Symbols](https://developer.apple.com/sf-symbols/) app.
- */
-const MAPPING: Record<string, MaterialIconName> = {
-  "house.fill": "home",
-  "paperplane.fill": "send",
-  "arrow.up": "arrow-upward",
-  "chevron.left.forwardslash.chevron.right": "code",
-  "chevron.right": "chevron-right",
-  "chevron.left": "chevron-left",
-  "globe.europe.africa.fill": "public",
-  "eye.fill": "visibility",
-  "eye.slash.fill": "visibility-off",
-  "mappin.and.ellipse": "place",
-  "flag.fill": "flag",
-};
-
-/**
- * An icon component that uses native SF Symbols on iOS, and Material Icons on Android and web.
- * This ensures a consistent look across platforms, and optimal resource usage.
- * Icon `name`s are based on SF Symbols and require manual mapping to Material Icons.
- */
 export function IconSymbol({
   name,
   size = 24,
-  color,
+  color = "white",
   style,
+  weight = "regular",
 }: {
-  readonly name: string;
+  readonly name: SymbolViewProps["name"];
   readonly size?: number;
-  readonly color: string | OpaqueColorValue;
-  readonly style?: StyleProp<TextStyle>;
+  readonly color?: string;
+  readonly style?: StyleProp<ViewStyle>;
+  readonly weight?: SymbolWeight;
 }) {
   return (
-    <MaterialIcons
-      color={color}
-      size={size}
-      name={MAPPING[name] ?? "help-outline"}
-      style={style}
+    <SymbolView
+      weight={weight}
+      tintColor={color}
+      resizeMode="scaleAspectFit"
+      name={name}
+      style={[
+        {
+          width: size,
+          height: size,
+        },
+        style,
+      ]}
     />
   );
 }
